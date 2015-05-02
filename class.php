8166bccab16b288d0878a5e1bc28ae3d9b43812f
Marco Meding <gosant@gmx.de>
2015-05-02 18:00:49 +0200

    ja?

---
+++ class.php
@@ -0,0 +1,20 @@
+<?php
+
+class spiel {
+    public $team1;
+    public $team2;
+    public $toreteam1;
+    public $toreteam2;
+
+    public function tor ($team) {
+        if ($team == $team1) {
+            $toreteam1++;
+        }
+        else {
+            $toreteam2++;
+        }
+
+    }
+}
+
+?>
